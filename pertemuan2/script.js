e0644372bddced3794f702e78331255126b0b640
GavraAM91 <125192790+GavraAM91@users.noreply.github.com>
2024-09-09 13:05:33 +0700

    progress belajar jquery

---
+++ pertemuan2/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+    //selector tag
+    $('h1').css('color', 'red');
+    $('p').css('color', 'blue');
+
+    //selector class 
+    $('.judul').css('background-color', 'red');
+    $('.paragraf').css('color', 'salmon');
+
+    //selector id
+    $('#judul').css('border', '2px solid salmon');
+    $('#paragraf').css('border', '2px solid blue');
+} );
